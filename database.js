8c9f6b5bca4fea75c98e7dd62e9c1a6dfada7bea
harshita133 <harshitaj270@gmail.com>
2024-09-18 01:29:33 -0400

    cac

--- database.js
+++ database.js
@@ -5,7 +5,10 @@ const pool=new Pool({
     password:"pd3d9f4aec4901e121561adb29d6d7fa0d9bc88914acf1475ac6d27baeaf15529",
     host:"c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
     port:5432,
-    database:"d435bkr679d42f"
+    database:"d435bkr679d42f",
+    ssl: {
+        rejectUnauthorized: false
+      }
 })
 
 
